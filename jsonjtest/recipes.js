35e40dfd119a2c721422f38a9a13c2bef9f5c0a8
jbou-nahra <40627152@live.napier.ac.uk>
2023-04-16 14:00:10 -0600

    Update recipes.js

--- jsonjtest/recipes.js
+++ jsonjtest/recipes.js
@@ -10,7 +10,7 @@ function loadQuestions() {
         console.log('error: JSON not loaded'); 
     })
     .done(function() {
-        console.log(allQuestions[0]);
+        console.log(allQuestions);
         //printQuestion(allQuestions[0]); 
     });
 }
