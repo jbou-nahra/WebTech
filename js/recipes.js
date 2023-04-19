8f19e171fa7bb9d10e66aef82cb4161fe3201ea6
jbou-nahra <40627152@live.napier.ac.uk>
2023-04-19 17:42:25 -0600

    Update recipes.js

--- js/recipes.js
+++ js/recipes.js
@@ -36,7 +36,7 @@ $.getJSON('recipes.json', function (data)
 		console.log("in for loop");
 		const card = recipecardtemplate.content.cloneNode(true).children[0];
 		const recipename = card.querySelector("[data-recipename]");
-		const recipelink = card.querySelector("[data-recipelink]");
+		var recipelink = card.querySelector("[data-recipelink]");
 		recipename.textContent = allRecipes[i].name;
 		var recipelinktemp = allRecipes[i].alink;
 		recipelink = recipelinktemp.link(URL);
